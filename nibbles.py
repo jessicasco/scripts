aa80897fa7073e444e23da726705fc7f71dc856d
Long Qian <qianlongzju@gmail.com>
2011-08-15 12:57:09 +0800

    change nibbles.py
    1. use curses.wrapper
    2. use class

--- nibbles.py
+++ nibbles.py
@@ -1,214 +1,191 @@
 #!/usr/bin/env python
-#coding=utf-8
 import curses
 import time
 import random
 
-stdscr      = None
-dashboard   = None
-nibbles     = []
-apple       = None
-score       = 0
-speed       = 1
+class Nibbles:
+    def __init__(self, stdscr):
+        curses.curs_set(0)
+        curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+        curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
+        height, width = stdscr.getmaxyx()
+        self.playboard = stdscr.subwin(height - 3, width, 0, 0)
+        self.playboard.keypad(1)
+        self.playboard.nodelay(1)
+        self.dashboard = stdscr.subwin(3, width, height - 3, 0)
+        self.dashboard.keypad(1)
+        self.dashboard.nodelay(1)
+        self.play()
 
-def init():
-    global stdscr, dashboard, nibbles, apple, score, speed
-    screen = curses.initscr()
-    y, x = screen.getmaxyx()
-    stdscr = screen.subwin(y-3, x, 0, 0)
-    stdscr.keypad(1)
-    stdscr.nodelay(1)
-    dashboard = screen.subwin(3, x, y-3, 0)
-    dashboard.keypad(1)
-    dashboard.nodelay(1)
-    curses.noecho()
-    curses.cbreak()
-    curses.curs_set(0)
-    curses.start_color()
-    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    def change_direction(self, c):
+        d = self.nibbles[0][-1]
+        if (c == ord('h')) or (c == curses.KEY_LEFT):
+            if d == 'left' or d == 'right':
+                pass
+            else:
+                self.nibbles[0][-1] = 'left'
+        elif (c == ord('j')) or (c == curses.KEY_DOWN):
+            if  d == 'up' or d == 'down':
+                pass
+            else:
+                self.nibbles[0][-1] = 'down'
+        elif (c == ord('k')) or (c == curses.KEY_UP):
+            if d == 'up' or d == 'down':
+                pass
+            else:
+                self.nibbles[0][-1] = 'up'
+        elif (c == ord('l')) or (c == curses.KEY_RIGHT):
+            if d == 'left' or d == 'right':
+                pass
+            else:
+                self.nibbles[0][-1] = 'right'
+        elif c == ord('q'):
+            self.quit()
+        elif c == ord(' '):
+            self.pause()
+        else:
+            pass
+        
+    def move_forward(self):
+        x, y, d = self.nibbles[0][0], self.nibbles[0][1], self.nibbles[0][2]
+        if d == 'up':
+            self.nibbles.insert(0, [x, y-1, d])
+            self.playboard.addstr(self.nibbles[0][1], self.nibbles[0][0], 
+                    '^', (curses.A_BOLD | curses.color_pair(1)))
+        elif d == 'down':
+            self.nibbles.insert(0, [x, y+1, d])
+            self.playboard.addstr(self.nibbles[0][1], self.nibbles[0][0],
+                    'v', (curses.A_BOLD | curses.color_pair(1)))
+        elif d == 'left':
+            self.nibbles.insert(0, [x-1, y, d])
+            self.playboard.addstr(self.nibbles[0][1], self.nibbles[0][0], 
+                    '<', (curses.A_BOLD | curses.color_pair(1)))
+        elif d == 'right':
+            self.nibbles.insert(0, [x+1, y, d])
+            self.playboard.addstr(self.nibbles[0][1], self.nibbles[0][0],
+                    '>', (curses.A_BOLD | curses.color_pair(1)))
+        else:
+            pass
+        self.playboard.addstr(self.nibbles[-1][1], self.nibbles[-1][0], ' ')
+        self.nibbles = self.nibbles[:-1]
+    
+    def place_an_apple(self):
+        flag = True
+        height, width = self.playboard.getmaxyx()
+        while flag:
+            x, y = random.randint(1, width - 2), random.randint(1, height - 2)
+            if x == self.apple[0] and y == self.apple[1]:
+                continue
+            for nibble in self.nibbles:
+                a, b = nibble[0], nibble[1]
+                if (a == x) and (b == y):
+                    break
+            else:
+                flag = False
+            self.apple = [x, y]
+            self.playboard.addstr(self.apple[1], self.apple[0], '*', (
+                curses.A_BOLD | curses.color_pair(2)))
+    
+    def judge(self):
+        x, y, d = self.nibbles[0][0], self.nibbles[0][1], self.nibbles[0][2]
+        height, width = self.playboard.getmaxyx()
+        if x == self.apple[0] and y == self.apple[1]:
+            self.score += 1
+            self.speed = (self.score / 10) + 1;
+            self.dashboard.addstr(1, 3, 'score: %s' % self.score, (
+                curses.A_REVERSE | curses.A_BOLD | curses.color_pair(3)))
+            self.dashboard.addstr(1, 15, 'speed: %s' % self.speed, (
+                curses.A_REVERSE | curses.A_BOLD | curses.color_pair(3)))
+            if d == 'up':
+                self.nibbles.insert(0, [x, y-1, d])
+                self.playboard.addstr(self.nibbles[0][1], self.nibbles[0][0],
+                        '^', (curses.A_BOLD |  curses.color_pair(1)))
+            elif d == 'down':
+                self.nibbles.insert(0, [x, y+1, d])
+                self.playboard.addstr(self.nibbles[0][1], self.nibbles[0][0],
+                        'v', (curses.A_BOLD | curses.color_pair(1)))
+            elif d == 'left':
+                self.nibbles.insert(0, [x-1, y, d])
+                self.playboard.addstr(self.nibbles[0][1], self.nibbles[0][0],
+                        '<', (curses.A_BOLD | curses.color_pair(1)))
+            elif d == 'right':
+                self.nibbles.insert(0, [x+1, y, d])
+                self.playboard.addstr(self.nibbles[0][1], self.nibbles[0][0],
+                        '>', (curses.A_BOLD | curses.color_pair(1)))
+            else:
+                pass
+            self.place_an_apple()
+        elif (x == 0) or (x == width - 1) or (y == 0) or (y == height - 1):
+            self.replay()
+        else:
+            pass
+    
+    def replay(self):
+        self.playboard.erase()
+        self.playboard.border(0)
+        self.playboard.refresh()
+        y, x = self.playboard.getmaxyx()
+        s = "Press 'q' to quit game,"
+        self.playboard.addstr(y/2, x/2 - len(s)/2, s, (curses.A_BOLD |
+            curses.color_pair(2)))
+        s = "others to replay."
+        self.playboard.addstr(y/2+1, x/2 - len(s)/2, s, (curses.A_BOLD |
+            curses.color_pair(2)))
+        self.playboard.nodelay(0)
+        c = self.playboard.getch()
+        if c == ord('q'):
+            self.quit()
+        else:
+            self.playboard.nodelay(1)
+            self.play()
+    
+    def play(self):
+        self.playboard.erase()
+        self.playboard.border(0)
+        self.dashboard.erase()
+        self.dashboard.border(0)
+        self.nibbles    = []
+        self.score      = 0
+        self.speed      = 1
+        y, x = self.playboard.getmaxyx()
+        self.apple = [int(x/2), int(y/2)]
+        self.nibbles.append([int(x/4), int(y/2), 'right'])
+        self.playboard.addstr(self.apple[1], self.apple[0], '*', (
+            curses.A_BOLD | curses.color_pair(2)))
+        self.playboard.addstr(self.nibbles[0][1], self.nibbles[0][0], '>', (
+            curses.A_BOLD | curses.color_pair(1)))
+        self.dashboard.addstr(1, 3, 'score: %s' % self.score, (
+            curses.A_REVERSE | curses.A_BOLD | curses.color_pair(3)))
+        self.dashboard.addstr(1, 15, 'speed: %s' % self.speed, (
+            curses.A_REVERSE | curses.A_BOLD | curses.color_pair(3)))
+        while True:
+            c = self.playboard.getch()
+            self.change_direction(c)
+            self.move_forward()
+            try:
+                self.judge()
+            except:
+                break
+            self.playboard.refresh()
+            self.dashboard.refresh()
+            time.sleep(0.1 / self.speed)
+        self.quit()
 
-def quit():
-    global stdscr, dashboard, nibbles, apple, score, speed
-    curses.nocbreak()
-    stdscr.keypad(0)
-    curses.echo()
-    if not curses.endwin():
+    def pause(self):
+        self.playboard.nodelay(0)
+        while True:
+            c = self.playboard.getch()
+            if c == ord(' '):
+                self.playboard.nodelay(1)
+                return
+
+    def quit(self):
         import sys
         sys.exit(0)
 
-def adjust(c):
-    global stdscr, dashboard, nibbles, apple, score, speed
-    a, b, d = nibbles[0][0], nibbles[0][1], nibbles[0][2]
-    if (c == ord('h')) or (c == curses.KEY_LEFT):
-        if d == 'left' or d == 'right':
-            pass
-        else:
-            nibbles[0][-1] = 'left'
-    elif (c == ord('j')) or (c == curses.KEY_DOWN):
-        if  d == 'up' or d == 'down':
-            pass
-        else:
-            nibbles[0][-1] = 'down'
-    elif (c == ord('k')) or (c == curses.KEY_UP):
-        if d == 'up' or d == 'down':
-            pass
-        else:
-            nibbles[0][-1] = 'up'
-    elif (c == ord('l')) or (c == curses.KEY_RIGHT):
-        if d == 'left' or d == 'right':
-            pass
-        else:
-            nibbles[0][-1] = 'right'
-    elif c == ord('q'):
-        quit()
-    elif c == ord(' '):
-        pause()
-    else:
-        pass
-
-def pause():
-    global stdscr, dashboard, nibbles, apple, score, speed
-    stdscr.nodelay(0)
-    while True:
-        c = stdscr.getch()
-        if c == ord(' '):
-            stdscr.nodelay(1)
-            break
-    
-def move():
-    global stdscr, dashboard, nibbles, apple, score, speed
-    a, b, d = nibbles[0][0], nibbles[0][1], nibbles[0][2]
-    if d == 'up':
-        nibbles.insert(0, [a, b-1, d])
-        stdscr.addstr(nibbles[0][1], nibbles[0][0], '^', (curses.A_BOLD |
-            curses.color_pair(1)))
-    elif d == 'down':
-        nibbles.insert(0, [a, b+1, d])
-        stdscr.addstr(nibbles[0][1], nibbles[0][0], 'v', (curses.A_BOLD |
-            curses.color_pair(1)))
-    elif d == 'left':
-        nibbles.insert(0, [a-1, b, d])
-        stdscr.addstr(nibbles[0][1], nibbles[0][0], '<', (curses.A_BOLD |
-            curses.color_pair(1)))
-    elif d == 'right':
-        nibbles.insert(0, [a+1, b, d])
-        stdscr.addstr(nibbles[0][1], nibbles[0][0], '>', (curses.A_BOLD |
-            curses.color_pair(1)))
-    else:
-        pass
-    stdscr.addstr(nibbles[-1][1], nibbles[-1][0], ' ')
-    nibbles = nibbles[:-1]
-
-def rearrangeApple(x, y):
-    global stdscr, dashboard, nibbles, apple, score, speed
-    flag = True
-    while flag:
-        a, b = random.randint(1, x-2), random.randint(1, y-2)
-        if a == apple[0] and b == apple[1]:
-            continue
-        for nibble in nibbles:
-            aa, bb = nibble[0], nibble[1]
-            if (aa == a) and (bb == b):
-                break
-        else:
-            flag = False
-        apple = [a, b]
-        stdscr.addstr(apple[1], apple[0], '*', (curses.A_BOLD |
-            curses.color_pair(2)))
-
-def judge(x, y):
-    global stdscr, dashboard, nibbles, apple, score, speed
-    a, b, d = nibbles[0][0], nibbles[0][1], nibbles[0][2]
-    if a == apple[0] and b == apple[1]:
-        score += 1
-        speed = (score / 10) + 1;
-        dashboard.addstr(1, 3, 'score: %s' % score, (curses.A_REVERSE |
-                curses.A_BOLD | curses.color_pair(3)))
-        dashboard.addstr(1, 15, 'speed: %s' % speed, (curses.A_REVERSE |
-                curses.A_BOLD | curses.color_pair(3)))
-        if d == 'up':
-            nibbles.insert(0, [a, b-1, d])
-            stdscr.addstr(nibbles[0][1], nibbles[0][0], '^', (curses.A_BOLD
-                |  curses.color_pair(1)))
-        elif d == 'down':
-            nibbles.insert(0, [a, b+1, d])
-            stdscr.addstr(nibbles[0][1], nibbles[0][0], 'v', (curses.A_BOLD
-                | curses.color_pair(1)))
-        elif d == 'left':
-            nibbles.insert(0, [a-1, b, d])
-            stdscr.addstr(nibbles[0][1], nibbles[0][0], '<', (curses.A_BOLD
-                | curses.color_pair(1)))
-        elif d == 'right':
-            nibbles.insert(0, [a+1, b, d])
-            stdscr.addstr(nibbles[0][1], nibbles[0][0], '>', (curses.A_BOLD
-                | curses.color_pair(1)))
-        else:
-            pass
-        rearrangeApple(x, y)
-    elif (a == 0) or (a == x - 1) or (b == 0) or (b == y-1):
-        restart()
-    else:
-        pass
-
-def restart():
-    global stdscr, dashboard, nibbles, apple, score, speed
-    stdscr.erase()
-    stdscr.border(0)
-    stdscr.refresh()
-    y, x = stdscr.getmaxyx()
-    s = "Press 'q' to quit game,"
-    stdscr.addstr(y/2, x/2 - len(s)/2, s, (curses.A_BOLD |
-        curses.color_pair(2)))
-    s = "others to replay."
-    stdscr.addstr(y/2+1, x/2 - len(s)/2, s, (curses.A_BOLD |
-        curses.color_pair(2)))
-    stdscr.nodelay(0)
-    c = stdscr.getch()
-    if c == ord('q'):
-        quit()
-    else:
-        stdscr.nodelay(1)
-        game()
-
-def game():
-    global stdscr, dashboard, nibbles, apple, score, speed
-    nibbles     = []
-    apple       = None
-    score       = 0
-    speed       = 1
-    stdscr.erase()
-    stdscr.border(0)
-    dashboard.erase()
-    dashboard.border(0)
-    y, x = stdscr.getmaxyx()
-    apple = [int(x/2), int(y/2)]
-    stdscr.addstr(apple[1], apple[0], '*', (curses.A_BOLD |
-        curses.color_pair(2)))
-    nibbles.append([int(x/4), int(y/2), 'right'])
-    stdscr.addstr(nibbles[0][1], nibbles[0][0], '>', (curses.A_BOLD |
-        curses.color_pair(1)))
-    dashboard.addstr(1, 3, 'score: %s' % score, (curses.A_REVERSE |
-        curses.A_BOLD | curses.color_pair(3)))
-    dashboard.addstr(1, 15, 'speed: %s' % speed, (curses.A_REVERSE |
-                curses.A_BOLD | curses.color_pair(3)))
-    while True:
-        c = stdscr.getch()
-        adjust(c)
-        move()
-        try:
-            judge(x, y)
-        except:
-            break
-        stdscr.refresh()
-        dashboard.refresh()
-        time.sleep(0.1/speed)
-    quit()
-
 def main():
-    init()
-    game()
+    curses.wrapper(Nibbles)
 
 if __name__ == '__main__':
     main()
